691f9ba03860d5808ba542844d27b7666e600f61
LamontSims11 <117595824+LamontSims11@users.noreply.github.com>
2022-11-07 18:49:25 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,21 +10,22 @@ int main()
     int c;
     double root1;
     double root2;
+    double singleroot;
 
     cout << "Enter the values for A, B, C" << endl;
     cin >> a >> b >> c;
 
    root1 = (-b + sqrt(pow(b,2)- 4 * a * c)) / (2 * a);
    root2 = (-b - sqrt(pow(b,2)- 4 * a * c)) / (2 * a);
+   singleroot = (-b + sqrt(pow(b,2)- 4 * a * c)) / (2 * a);
 
-   if(pow(b, 2) - 4 * a * c < 0)
+   if(pow(b,2) - 4 * a * c < 0)
      cout << " complex roots " << endl;
-   if (pow(b, 2) - 4 * a * c > 0)
-     cout << " two real roots " << endl;
-   if (pow(b, 2) - 4 * a * c == 0)
-     cout << " one real root " << endl;
-    
- cout << " the 1st root is " << root1 << endl; 
- cout << " the 2nd root is " << root2 << endl; 
+   if (pow(b,2) - 4 * a * c > 0)
+     cout << " two real roots " << " the first root is " << root1 
+     << " the second root is " << root2 << endl;
+   if (pow(b,2) - 4 * a * c == 0)
+     cout << " one real root " << " single root is "
+     << singleroot << endl;
  return 0;
 }
